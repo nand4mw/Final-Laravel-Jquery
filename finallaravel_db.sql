0f74e6f9472088efb895564b1d787f1494300be6
nand4mw <ananda1850mw@gmail.com>
2024-01-11 01:29:30 +0700

    add sql

--- finallaravel_db.sql
+++ finallaravel_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 10, 2024 at 04:28 PM
+-- Generation Time: Jan 10, 2024 at 06:28 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
